0c5f35d1aff5a8b569246839b13e2c7ba47680cf
Dipak Paswan <080bel015@ioepc.edu.np>
2025-12-04 13:36:15 +0545

    Added a new file to express happiness

---
+++ welcome.py
@@ -0,0 +1,3 @@
+#This the new python file saying welcome because for the first time I have pushed local repository to remote repository
+print("Welcome to the gitandgithub-learn repository!")
+print("I have successfully pushed my local repository to the remote repository on GitHub.")
