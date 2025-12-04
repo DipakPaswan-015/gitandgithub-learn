828a54ec8691fceb90c9c19944f0b48b6e8fa2b7
Dipak Paswan <080bel015@ioepc.edu.np>
2025-12-04 12:46:16 +0545

    some changes added in the first.py file

---
+++ first.py
@@ -0,0 +1,3 @@
+#This is the first python file in the gitandgithub-learn repository
+print("Hello, welcome to the gitandgithub-learn repository!")
+print("This is my first Python file in this repository.")
